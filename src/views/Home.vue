b48edf225317a030110dda00d0e1d56e8d80d867
Hélio M S Olivas <heliomsolivas@gmail.com>
2022-05-02 17:23:15 -0300

    Delete & Done methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,45 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @deleteProject="handleDelete"
+          @complete="handleComplete"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SingleProject from "../components/SingleProject.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((s) => s.id !== id);
+    },
+    handleComplete(id) {
+      let p = this.projects.find((s) => {
+        return s.id === id;
+      });
+      p.complete = !p.complete;
+    },
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
